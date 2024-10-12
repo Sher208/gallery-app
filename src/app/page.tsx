0346c924203af97e59f9bfae2a461b60b957fbf3
Sher208 <thilakrajshetty11@gmail.com>
2024-10-13 00:33:02 +0530

    scaffolding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,25 @@
-import Link from "next/link";
+import Image from "next/image";
+
+const imagesLink = [
+  "https://utfs.io/f/QOg6WchRNpxEpdffs67sXL1ikjQzmGWqvTU9DdxVMory3nNI",
+  "https://utfs.io/f/QOg6WchRNpxEgXFlDYrrP1qtcF2Y4msIuwejyAWxXhp5GUE8",
+];
+
+const getImages = imagesLink.map((link, index) => ({
+  id: index + 1,
+  imageUrl: link,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
-    </main>
+    <div className="flex flex-wrap gap-10">
+      {getImages.map((image) => {
+        return (
+          <div key={image.id}>
+            <Image width={400} height={400} src={image.imageUrl} alt="image" />
+          </div>
+        );
+      })}
+    </div>
   );
 }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="flex w-full items-center justify-between border-b border-red-50 p-4 text-xl font-semibold">
+      <div>This is a gallery app</div>
+      <div>Sign In</div>
+    </div>
+  );
+}
